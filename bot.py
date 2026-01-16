cd8ef171f267f45239c1323e40a95e2f26972855
homoxx10-wq <homoxx10@gmail.com>
2026-01-16 10:43:24 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-BOT_TOKEN = "8291160805:AAFGCJLQngyFsVDJGQjkP_qnH2GOIDuDI68"  # <-- Replace this with the token you got from BotFather
+"  # <-- Replace this with the token you got from BotFather
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I am your Turn Bot!")
